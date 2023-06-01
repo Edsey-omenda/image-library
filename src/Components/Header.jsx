481ce84aeed1e70c388fcffeda59f6a80f6890a6
Edsey-omenda <omendajames0317@gmail.com>
2023-06-01 20:07:19 +0300

    adds salesatwork logo to the assets folder

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Header = ({ title }) => {
     return (
         <header>
-            {}
+          <a href="https://vitejs.dev" target="_blank">
+           <img src={viteLogo} className="logo" alt="Salesatwork logo" />
+          </a>
             <h2>{title}</h2>
         </header>
     );
