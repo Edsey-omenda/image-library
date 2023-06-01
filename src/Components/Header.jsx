9fba7ba51550ce1a337dd8427250e3747cb4b749
Edsey-omenda <omendajames0317@gmail.com>
2023-06-01 20:10:51 +0300

    imports salesatwork logo and render it in the header component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import salesatwork from "../assets/salesatwork.png"
 
 const Header = ({ title }) => {
     return (
         <header>
           <a href="https://vitejs.dev" target="_blank">
-           <img src={viteLogo} className="logo" alt="Salesatwork logo" />
+           <img src={salesatwork} className="logo" alt="Salesatwork logo" />
           </a>
             <h2>{title}</h2>
         </header>
